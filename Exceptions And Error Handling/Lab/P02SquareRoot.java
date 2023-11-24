8cfb39d68e363aa4db37b11b976d8cfa1ec5c315
Radostin Stoyanov <127531328+RadostinStoyanov1@users.noreply.github.com>
2023-11-25 00:35:16 +0200

    Add files via upload

---
+++ Exceptions And Error Handling/Lab/P02SquareRoot.java
@@ -0,0 +1,25 @@
+package P17ExceptionsAndErrorHandling;
+
+import java.util.Scanner;
+
+public class P02SquareRoot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        try {
+            int number = Integer.parseInt(input);
+            if (number >= 0) {
+                double sqrt = Math.sqrt(number);
+                System.out.printf("%.2f\n", sqrt);
+            } else {
+                System.out.println("Invalid");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid");
+        } finally {
+            System.out.println("Goodbye");
+        }
+    }
+}
